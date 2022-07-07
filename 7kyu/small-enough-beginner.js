e2c382c8f706e1eb54717f4572485e6a9604d1eb
Aslane Abou Karroum <aslaneak153@gmail.com>
2022-07-07 16:19:45 +0300

    added new codewars challenge

---
+++ 7kyu/small-enough-beginner.js
@@ -0,0 +1,13 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+// The solution
+function smallEnough(a, limit){
+    return Math.max(...a) <= limit
+  }
+
+// The solution 2
+function smallEnough(a, limit){
+    return a.filter(num=>num>limit).length ? false : true
+  }
