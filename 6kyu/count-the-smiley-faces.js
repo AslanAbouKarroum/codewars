cede39d4f60824e4605ee557d9fc65da99a4e617
Aslane Abou Karroum <aslaneak153@gmail.com>
2022-07-27 20:02:41 +0300

    added new codewars challenge

---
+++ 6kyu/count-the-smiley-faces.js
@@ -0,0 +1,35 @@
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// Example
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+// The solution
+function countSmileys(arr) {
+    return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
+// The solution 2
+function countSmileys(arr) {
+  return arr.reduce((acc,el)=>{
+    if(el.length == 3){
+      el[0].match(/[;:]/) && el[1].match(/[-~]/) && el[2].match(/[)D]/) ? ++acc :null
+    }else if(el.length == 2){
+      el[0].match(/[;:]/) && el[1].match(/[)D]/)? ++acc : null
+    }else{
+      null
+    }
+    return acc
+    },0)
+}
