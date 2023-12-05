35af0cc163931cb9b2ab2ec00a594311a27a6cd1
AslanAbouKarroum <aslaneak153@gmail.com>
2023-12-05 18:23:31 +0200

    added new codewars challenge

---
+++ 6kyu/primorial-of-a-number.js
@@ -0,0 +1,39 @@
+// Definition (Primorial Of a Number)
+// Is similar to factorial of a number, In primorial, not all the natural numbers get multiplied, only prime numbers are multiplied to calculate the primorial of a number. It's denoted with P# and it is the product of the first n prime numbers.
+
+// Task
+// Given a number N , calculate its primorial.!alt!alt
+// Notes
+// Only positive numbers will be passed (N > 0) .
+// Input >> Output Examples:
+// 1- numPrimorial (3) ==> return (30)
+// Explanation:
+// Since the passed number is (3) ,Then the primorial should obtained by multiplying 2 * 3 * 5 = 30 .
+
+// Mathematically written as , P3# = 30 .
+// 2- numPrimorial (5) ==> return (2310)
+// Explanation:
+// Since the passed number is (5) ,Then the primorial should obtained by multiplying  2 * 3 * 5 * 7 * 11 = 2310 .
+
+// Mathematically written as , P5# = 2310 .
+// 3- numPrimorial (6) ==> return (30030)
+// Explanation:
+// Since the passed number is (6) ,Then the primorial should obtained by multiplying  2 * 3 * 5 * 7 * 11 * 13 = 30030 .
+
+// Mathematically written as , P6# = 30030 .
+
+
+// My Solution
+
+function numPrimorial(n){
+    let arr =[2,3,5,7,11,13,17,19,23];
+    let j=29;
+    while(arr.length<n){ 
+        for (let i = 2; i <= j; i++) {
+            if(i==j) arr.push(j)
+            if (j%i === 0) break;
+        }
+        j=j+2;
+    };
+    return arr.slice(0,n).reduce((acc,num)=>acc*num)
+};
