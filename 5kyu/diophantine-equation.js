693b58dd6de2cf4fe368c996726b3b4cb16e370f
AslanAbouKarroum <aslaneak153@gmail.com>
2024-03-06 23:54:42 +0200

    added new codewars challenge

--- 5kyu/diophantine-equation.js
+++ 5kyu/diophantine-equation.js
@@ -13,4 +13,13 @@
 // Hint:
 // x2 - 4 * y2 = (x - 2*y) * (x + 2*y)
 
-// My solution
+// Solution
+function solequa(n) {
+    let result = []
+    for ( let d, x, y, i=1; i <= Math.sqrt(n); i++ ) {
+      if ( Number.isInteger( d = n/i ) && Number.isInteger( x = (d+i)/2) && Number.isInteger( y = (d-i)/4)) {
+        result.push([x,y]);
+      }
+    }
+    return result;
+};
